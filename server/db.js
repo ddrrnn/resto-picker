196d2d24a7a1c5a498d933eab61c051794e1e42e
Duranne B. Duran <duranduranne@gmail.com>
2024-08-09 00:50:25 +0800

    vercel

--- server/db.js
+++ server/db.js
@@ -1,11 +1,7 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user:"postgres",
-    password:"rain51dbduran",
-    host:"localhost",
-    port: 5432,
-    database:"restopicker"
-});
+    connectionString: process.env.POSTGRES_URL,
+  })
 
 module.exports = pool;
